7d803139003066eefea80a13b88ef3a59390bcf8
Chemmuta Sireesha <90855264+Sireesha9715@users.noreply.github.com>
2021-09-17 00:27:51 +0530

    Add files via upload

---
+++ swt.sh
@@ -0,0 +1,16 @@
+cars="abcd"
+
+case $cars in
+	#case 1
+	"djjs" ) 
+	echo "this is not my car" ;;
+
+	#case 2
+	"dsad" ) 
+	echo "this is also not my car" ;;
+
+	#case 3
+	"abcd" ) 
+	echo "this is my car" ;;
+
+esac
